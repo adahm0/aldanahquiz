c1b83497cc29d63342d125e5868d4eb228f31a23
adahm0 <danah056524@gmail.com>
2024-12-17 17:34:22 +0300

    hi

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿public class test2
+{
+public static void Main()
+{
+//1
+string name = "aldanah alhazza";
+Console.WriteLine(name);
+//2
+int num1=int.Parse(Console.ReadLine());
+int num2 =2;
+Console.WriteLine(num2*num1);
+
+//3
+
+int[]index = {1,2,3,4,5};
+Console.WriteLine(index[0]);
+
+ 
+//4
+
+int con = int.Parse(Console.ReadLine());
+int num = 10;
+if(num >=con)
+{
+Console.WriteLine("good");
+}
+else
+{
+    Console.WriteLine("try agin");
+}
+
+//5
+int massg = int.Parse(Console.ReadLine());
+switch(massg){
+case 1:
+Console.WriteLine("hello");
+break;
+case 2:
+Console.WriteLine("welcom");
+break;
+case 3:
+Console.WriteLine("good");
+break;
+default:
+{
+    Console.WriteLine("tray agin");
+}break;
+
+}
+}
+}
